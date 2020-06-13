431c2b5844e022279ee55fef0bfa2b684f6c6052
Shounak <shounak.b95@gmail.com>
2020-06-12 20:43:14 -0700

    new lines added

--- src/myJavaProject/myAppTester.java
+++ src/myJavaProject/myAppTester.java
@@ -17,7 +17,7 @@ public class myAppTester {
 	  reverseMethod(n);
 	  System.out.println();
 
-	  // add a line below to call your myTestMethod from here  then remove the block comment symbols  
+	  myTestMethod();  
 
 	}
 	
@@ -34,9 +34,11 @@ public class myAppTester {
 	   }
 
 
-	   /* 
-	       add your test method in this space then remove the block comment symbols
-	       public static void myTestMethod(...){
+	  
+	       public static void myTestMethod(){
+	    	   System.out.println("testing method");
+	       
+	       }
 
             
 
